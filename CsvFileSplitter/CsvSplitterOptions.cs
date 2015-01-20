d11eeae1d943c5085e52bed14ccf2a520068eb3d
meziantou <meziantou@live.fr>
2015-01-20 23:19:14 +0100

    Initial commit

---
+++ CsvFileSplitter/CsvSplitterOptions.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Documents;
+using CsvFileSplitter.Annotations;
+
+namespace CsvFileSplitter
+{
+    public class CsvSplitterOptions : INotifyPropertyChanged, IDataErrorInfo
+    {
+        private string _inputFile;
+        private string _outputDirectory;
+        private string _outputFileFormat;
+        private long _linesByFile = 10000;
+        private long _headerLineCount = 1;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string InputFile
+        {
+            get { return _inputFile; }
+            set
+            {
+                if (value == _inputFile) return;
+                _inputFile = value;
+                OnPropertyChanged();
+
+                if (!string.IsNullOrEmpty(_inputFile))
+                {
+                    var directoryName = Path.GetDirectoryName(_inputFile);
+                    if (OutputDirectory == null)
+                    {
+                        OutputDirectory = directoryName;
+                    }
+
+                    var fileName = Path.GetFileNameWithoutExtension(_inputFile);
+                    var extension = Path.GetExtension(_inputFile);
+                    OutputFileFormat = fileName + "_{0:#000}" + extension;
+                }
+            }
+        }
+
+        public string OutputDirectory
+        {
+            get { return _outputDirectory; }
+            set
+            {
+                if (value == _outputDirectory) return;
+                _outputDirectory = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public string OutputFileFormat
+        {
+            get { return _outputFileFormat; }
+            set
+            {
+                if (value == _outputFileFormat) return;
+                _outputFileFormat = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long LinesByFile
+        {
+            get { return _linesByFile; }
+            set
+            {
+                if (value == _linesByFile) return;
+                _linesByFile = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public long HeaderLineCount
+        {
+            get { return _headerLineCount; }
+            set
+            {
+                if (value == _headerLineCount) return;
+                _headerLineCount = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                List<string> errors = new List<string>();
+
+                if (string.IsNullOrEmpty(columnName) || columnName == "InputFile")
+                {
+                    if (string.IsNullOrEmpty(InputFile))
+                        errors.Add("Input file must be set.");
+                    else if (!File.Exists(InputFile))
+                        errors.Add("Input file not found.");
+                }
+
+                if (string.IsNullOrEmpty(columnName) || columnName == "OutputDirectory")
+                {
+                    if (string.IsNullOrEmpty(OutputDirectory))
+                        errors.Add("Output directory must be set.");
+                    else if (!Directory.Exists(OutputDirectory))
+                        errors.Add("Output directory not found.");
+                }
+
+                if (string.IsNullOrEmpty(columnName) || columnName == "OutputFileFormat")
+                {
+                    if (string.IsNullOrEmpty(OutputFileFormat))
+                        errors.Add("Output file format must be set.");
+                }
+
+                if (errors.Any())
+                    return string.Join(Environment.NewLine, errors);
+                return null;
+            }
+        }
+
+        public string Error
+        {
+            get { return this[null]; }
+        }
+    }
+}
---
+++ CsvFileSplitter/MainWindow.xaml.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Forms;
+using DataFormats = System.Windows.DataFormats;
+using DragEventArgs = System.Windows.DragEventArgs;
+using MessageBox = System.Windows.MessageBox;
+using OpenFileDialog = Microsoft.Win32.OpenFileDialog;
+
+namespace CsvFileSplitter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly CsvSplitterOptions _csvSplitterOptions;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            _csvSplitterOptions = new CsvSplitterOptions();
+            DataContext = _csvSplitterOptions;
+        }
+
+        private void ButtonInputFile_OnClick(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+            {
+                if (!string.IsNullOrEmpty(dialog.FileName))
+                {
+                    _csvSplitterOptions.InputFile = dialog.FileName;
+                }
+            }
+        }
+
+        private void ButtonOutputDirectory_OnClick(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            var dialogResult = dialog.ShowDialog();
+            if (dialogResult == System.Windows.Forms.DialogResult.OK || dialogResult == System.Windows.Forms.DialogResult.Yes)
+            {
+                if (!string.IsNullOrEmpty(dialog.SelectedPath))
+                {
+                    _csvSplitterOptions.OutputDirectory = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private async void ButtonSplit_OnClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                ButtonSplit.IsEnabled = false;
+                await Split();
+            }
+            finally
+            {
+                ButtonSplit.IsEnabled = true;
+            }
+        }
+
+        private async Task Split()
+        {
+            var options = _csvSplitterOptions;
+            if (!string.IsNullOrEmpty(options.Error))
+            {
+                MessageBox.Show(options.Error, "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            try
+            {
+                using (var inputStream = new StreamReader(options.InputFile))
+                {
+                    string header = null;
+                    if (options.HeaderLineCount > 0)
+                    {
+                        TextBlockProgress.Text = "Reading header...";
+                        StringBuilder headerStringBuilder = new StringBuilder();
+                        for (int i = 0; i < options.HeaderLineCount && !inputStream.EndOfStream; i++)
+                        {
+                            if (i > 0)
+                            {
+                                headerStringBuilder.AppendLine();
+                            }
+                            headerStringBuilder.Append(await inputStream.ReadLineAsync());
+                        }
+
+                        header = headerStringBuilder.ToString();
+                    }
+
+                    int fileIndex = 0;
+                    while (!inputStream.EndOfStream)
+                    {
+                        // Create output file
+                        string outputFileName = string.Format(options.OutputFileFormat, fileIndex++);
+                        string outputPath = Path.Combine(options.OutputDirectory, outputFileName);
+                        TextBlockProgress.Text = string.Format("Creating part {0}...", fileIndex);
+                        using (StreamWriter writer = new StreamWriter(outputPath, false))
+                        {
+                            // Write header
+                            if (header != null)
+                            {
+                                TextBlockProgress.Text = string.Format("Part {0}: writing header...", fileIndex);
+                                await writer.WriteAsync(header);
+                            }
+
+                            // Write content
+                            for (int i = 0; i < options.LinesByFile && !inputStream.EndOfStream; i++)
+                            {
+                                if (i % 50 == 0)
+                                {
+                                    TextBlockProgress.Text = string.Format("Part {0}: writing line {1}...", fileIndex, i);
+                                }
+
+                                if (header != null || i > 0)
+                                {
+                                    await writer.WriteLineAsync();
+                                }
+                                string line = await inputStream.ReadLineAsync();
+                                await writer.WriteAsync(line);
+                            }
+                        }
+                    }
+                }
+
+                MessageBox.Show("File splitted successfully!", "Success", MessageBoxButton.OK);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString(), "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                TextBlockProgress.Text = string.Empty;
+            }
+        }
+
+        private void MainWindow_OnDrop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                string data = ((string[])e.Data.GetData(DataFormats.FileDrop)).FirstOrDefault();
+                if (data != null)
+                {
+                    if (File.Exists(data))
+                    {
+                        _csvSplitterOptions.InputFile = data;
+                    }
+                    else if (Directory.Exists(data))
+                    {
+                        _csvSplitterOptions.OutputDirectory = data;
+                    }
+                }
+            }
+        }
+    }
+}
